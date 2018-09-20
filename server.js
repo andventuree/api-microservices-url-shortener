65896f202c882fea8360e100f7cab91ae500f6bb
andventuree <awwong92@gmail.com>
2018-09-20 13:14:29 -0400

    added url validators

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var mongo = require("mongodb");
 var mongoose = require("mongoose");
 let bodyParser = require("body-parser");
 let Schema = mongoose.Schema;
+let dns = require("dns");
 
 // Alternative of using .env shell file to add on MONGOLAB_URI
 // This require stmt will populate process.env.MONGOLAB_URI
@@ -52,51 +53,64 @@ app.get("/api/shorturl", (req, res, next) => {
   // Find all instancess
   URL.find(data => data)
     .then(dbData => {
-      if (dbData.length > 0) {
-        return dbData.map((entry, idx) => {
-          return {
-            original_url: entry.original_url,
-            short_url: idx
-          };
-        });
-      } else {
-        return { submitted: [] };
-      }
+      if (dbData.length === 0) return [];
+      let urls = dbData.map((entry, idx) => {
+        return {
+          original_url: entry.original_url,
+          short_url: idx
+        };
+      });
+      return urls;
     })
-    .then(data => res.json(data))
+    .then(urls => res.json({ urls }))
     .catch(next);
 });
 
 // POST /api/shorturl/new
 app.post("/api/shorturl/new", (req, res, next) => {
-  // 1) Find collection with URL key so you can get next index
-  URL.find(data => data)
-    .then(dbData => {
-      console.log("dbData: ", dbData);
-      // 2) Regex to remove protocols e.g., https://
-      let sanitizedUrl = req.body.url.replace(/(^\w+:|^)\/\//, "");
-      // 3) Create instance to save to DB
-      let url = new URL({
-        original_url: sanitizedUrl,
-        short_url: dbData.length //next index
-      });
-      // 3) Save instance to DB
-      url.save((err, data) => {
-        if (err) next(err);
-        console.log("spot 2", data);
-        let { original_url, short_url } = data;
-        // 4) return saved results back to front end
-        res.json({ original_url, short_url });
-      });
-    })
-    .catch(next);
+  // 1) Regex to remove protocols e.g., https://
+  let sanitizedUrl = req.body.url.replace(/(^\w+:|^)\/\//, "");
+  // 2) Determine if URL is valid
+  dns.lookup(sanitizedUrl, (err, address, family) => {
+    if (err) {
+      // 2a) invalid URL, send error
+      res.json({ error: "invalid URL" });
+    } else {
+      // 2b) Find collection with URL key so you can get next index
+      URL.find(data => data)
+        .then(dbData => {
+          // 3) Create instance to save to DB
+          let url = new URL({
+            original_url: sanitizedUrl,
+            short_url: dbData.length //next index
+          });
+          // 4) Save instance to DB
+          url.save((err, data) => {
+            if (err) next(err);
+            let { original_url, short_url } = data;
+            // 5) Return saved results back to front end
+            res.json({ original_url, short_url });
+          });
+        })
+        .catch(next);
+    }
+  });
 });
 
 // GET /api/shorturl/:id
 app.get("/api/shorturl/:id", (req, res, next) => {
-  // URL.findById(req.params.id, data =>
-  //   res.json({ original_url: data.original_url, short_url: data.short_url })
-  // );
+  console.log(req.params.id);
+  URL.findOne({ short_url: req.params.id }, (err, data) => {
+    if (err) next(err);
+    data === null
+      ? res.json({
+          error: "No short url found for given input"
+        })
+      : res.json({
+          original_url: data.original_url,
+          short_url: data.short_url
+        });
+  }).catch(next);
 });
 
 // Start server
