82a8fdbf301f068f1c3b16ef9863f2aae0299303
andventuree <awwong92@gmail.com>
2018-09-20 11:52:52 -0400

    completed api/shorturl/new endpoint

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require("express");
 var mongo = require("mongodb");
 var mongoose = require("mongoose");
 let bodyParser = require("body-parser");
+let Schema = mongoose.Schema;
 
 // Alternative of using .env shell file to add on MONGOLAB_URI
 // This require stmt will populate process.env.MONGOLAB_URI
@@ -17,6 +18,8 @@ var app = express();
 // Basic Configuration
 var port = process.env.PORT || 3000;
 
+// Middleware
+
 /** this project needs a db !! **/
 mongoose.connect(process.env.MONGOLAB_URI);
 
@@ -29,14 +32,72 @@ app.use(bodyParser.json());
 
 app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get("/", function(req, res) {
+app.get("/", function(req, res, next) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// your first API endpoint...
-app.get("/api/hello", function(req, res) {
-  res.json({ greeting: "hello API" });
+// MongoDB models and schemas
+
+let urlSchema = new Schema({
+  original_url: { type: String, required: true },
+  short_url: Number
 });
+
+let URL = mongoose.model("url", urlSchema);
+
+// API endpoints
+
+// GET /api/shorturl
+app.get("/api/shorturl", (req, res, next) => {
+  // Find all instancess
+  URL.find(data => data)
+    .then(dbData => {
+      if (dbData.length > 0) {
+        return dbData.map((entry, idx) => {
+          return {
+            original_url: entry.original_url,
+            short_url: idx
+          };
+        });
+      } else {
+        return { submitted: [] };
+      }
+    })
+    .then(data => res.json(data))
+    .catch(next);
+});
+
+// POST /api/shorturl/new
+app.post("/api/shorturl/new", (req, res, next) => {
+  // 1) Find collection with URL key so you can get next index
+  URL.find(data => data)
+    .then(dbData => {
+      console.log("dbData: ", dbData);
+      // 2) Create instance to save to DB
+      let url = new URL({
+        original_url: req.body.url,
+        short_url: dbData.length //next index
+      });
+      // 3) Save instance to DB
+      url.save((err, data) => {
+        if (err) next(err);
+        console.log("spot 2", data);
+        let { original_url, short_url } = data;
+        // 4) return saved results back to front end
+        res.json({ original_url, short_url });
+      });
+    })
+    .catch(next);
+});
+
+// GET /api/shorturl/:id
+app.get("/api/shorturl/:id", (req, res, next) => {
+  // URL.findById(0, data =>
+  //   res.json({ original_url: data.original_url, short_url: data.short_url })
+  // );
+});
+
+// Start server
 
 app.listen(port, function() {
   console.log("Node.js listening ...", port);
